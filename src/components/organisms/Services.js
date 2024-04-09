1e19ac5f131bf5bce28ba9e2e1d8630ab271be3f
Carlos Barrios <juanbarrios045@gmail.com>
2024-04-09 17:32:03 -0400

    202415 | New images

--- src/components/organisms/Services.js
+++ src/components/organisms/Services.js
@@ -10,21 +10,21 @@ import Image from "next/image";
 const servicesData = [
   {
     title: "Choose the date",
-    image: "/images/calendar.svg",
+    image: "/images/step-1.jpg",
     description:
       "Discover the art of photography with our comprehensive courses. Whether you're a beginner or looking to enhance your skills, our experienced instructors will guide you through the technical aspects, composition techniques, and creative approaches to photography. Immerse yourself in a dynamic learning environment and unlock your full potential as a photographer. Join our courses and embark on a journey of visual storytelling.",
     button: "Step 1",
   },
   {
     title: "Personalize your event",
-    image: "/images/exposition.svg",
+    image: "/images/step-2.jpg",
     description:
       "Immerse yourself in the vibrant world of photography through our exciting events and contests. Join us for captivating photography expeditions, workshops, and exhibitions that showcase the beauty of different genres, from landscapes to portraits. Engage with fellow photography enthusiasts, learn from industry experts, and participate in our prestigious photography contests to showcase your talent and win exciting prizes. Ignite your creative passion and be part of our inspiring community.",
     button: "Step 2",
   },
   {
     title: "Enjoy your event",
-    image: "/images/event.svg",
+    image: "/images/step-3.jpg",
     description:
       "Immerse yourself in the vibrant world of photography through our exciting events and contests. Join us for captivating photography expeditions, workshops, and exhibitions that showcase the beauty of different genres, from landscapes to portraits. Engage with fellow photography enthusiasts, learn from industry experts, and participate in our prestigious photography contests to showcase your talent and win exciting prizes. Ignite your creative passion and be part of our inspiring community.",
     button: "Step 3",
@@ -40,25 +40,22 @@ const Services = () => {
         <div className="container mx-auto px-4 w-full h-full flex flex-col justify-center">
           <div className="grid grid-cols-3 gap-5 mt-20">
             {servicesData.map((item, i) => (
-              <div className="flex flex-col bg-white p-10" key={i}>
-                <h1 className="text-xl sm:text-2xl lg:text-3xl font-medium">
-                  {item.title}
-                </h1>
+              <div
+                className="bg-center bg-cover"
+                style={{ backgroundImage: `url(${item.image})` }}
+                key={i}
+              >
+                <div className="flex flex-col bg-black text-white bg-opacity-40 h-full w-full p-10">
+                  <h1 className="text-xl sm:text-2xl lg:text-3xl font-medium">
+                    {item.title}
+                  </h1>
 
-                <div className="mt-10 text-sm flex-1">
-                  {/* <p>{item.description}</p> */}
-                  <Image
-                    src={item.image}
-                    alt="Image service"
-                    width={200}
-                    height={200}
-                    className="h-full"
-                  />
+                  <div className="flex-1 mt-10 flex items-end">
+                    <Link href="/#shop">
+                      <Button label={item.button} />
+                    </Link>
+                  </div>
                 </div>
-
-                <Link className="mt-10" href="/#shop">
-                  <Button label={item.button} />
-                </Link>
               </div>
             ))}
           </div>
