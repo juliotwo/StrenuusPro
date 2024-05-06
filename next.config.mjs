11f542fdb51551a7d9c9234083a429719407a577
juliotwo <juliotwo1996@gmail.com>
2024-05-06 11:06:12 -0600

    Add api

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://backend.bstable.bitrus.com/:path*',
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
