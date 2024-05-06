2ebf70855c0a2f73edc80a4139f7c0a410ab2cf0
juliotwo <juliotwo1996@gmail.com>
2024-05-06 11:50:55 -0600

    Add api

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ import { TRANSACTION } from './urls';
 export const ApiTransaction = {
   makeTransaction: async (data) => {
     try {
-      const response = await axios.post(TRANSACTION, data, {
+      const response = await axios.post('/api/proxy', data, {
         headers: {
           'Content-Type': 'application/json',
           'MERCHANT-API-KEY': `6N3EO55S1n7IYCZuAoNRphyTm6Pzn0OoKrcMn4FneRYaYBO0WeaV3Co2bzYXKSwx`,
