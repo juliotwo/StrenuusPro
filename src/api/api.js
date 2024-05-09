6b17786aa49cd0bfe5c104b511a9fb53cd897b72
juliotwo <juliotwo1996@gmail.com>
2024-05-08 19:08:45 -0600

    Delete logs

--- src/api/api.js
+++ src/api/api.js
@@ -1,30 +1,18 @@
 import axios from 'axios';
 import { TRANSACTION } from './urls';
-const fetch = require('node-fetch');
 
 export const ApiTransaction = {
   makeTransaction: async (data) => {
     try {
-      // const response = await axios.post(TRANSACTION, data, {
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     'MERCHANT-API-KEY': `6N3EO55S1n7IYCZuAoNRphyTm6Pzn0OoKrcMn4FneRYaYBO0WeaV3Co2bzYXKSwx`,
-      //   },
-      // });
-      const response = await fetch(TRANSACTION, {
-        method: 'POST',
+      const response = await axios.post(TRANSACTION, data, {
         headers: {
           'Content-Type': 'application/json',
           'MERCHANT-API-KEY': `6N3EO55S1n7IYCZuAoNRphyTm6Pzn0OoKrcMn4FneRYaYBO0WeaV3Co2bzYXKSwx`,
         },
-        body: JSON.stringify(data),
       });
-      const datos = await response.json();
-      console.log(datos);
-      return datos;
+      return response.data;
     } catch (error) {
       console.log(error);
-      return error;
     }
   },
 };
