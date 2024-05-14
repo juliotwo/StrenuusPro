7408303888386a2afcf4026cb0f85b0e36e1fedc
juliotwo <juliotwo1996@gmail.com>
2024-05-14 11:59:58 -0600

    Fix

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://backend.bstable.bitrus.com/api/';
+export const BASE_URL = 'https://backend.dev.bstable.bitrus.com/api/';
 
 export const TRANSACTION = `${BASE_URL}v1/merchant_transaction/`;
