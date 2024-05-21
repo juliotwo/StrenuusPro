7d91375cb5ba28740aa8c3d9c8ea365b99f4fc0b
juliotwo <juliotwo1996@gmail.com>
2024-05-20 22:40:23 -0600

    Update

--- src/components/organisms/CartSection.js
+++ src/components/organisms/CartSection.js
@@ -1,48 +1,177 @@
-"use client";
-import { useState } from "react";
-import { useRouter } from "@/navigation";
+'use client';
+import { useContext, useState } from 'react';
+import { useRouter } from '@/navigation';
 
-import { useTranslations } from "next-intl";
-// import { ApiTransaction } from "@/api/api";
+import { useTranslations } from 'next-intl';
+import { ApiTransaction } from '@/api/api';
 // import { optionsStates, pageName, phoneNumber } from "@/data";
 // import { ProgressSpinner } from "primereact/progressspinner";
-import { CartSection, Button, Payments } from "ui-pages-ecommerce";
+import { CartSection, Button, Payments, CartContext } from 'ui-pages-ecommerce';
+import { pageName } from '@/data';
+
+const validDiscountCode = ['DISCOUNT10', 'DISCOUNT20'];
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 const CartSectionComponent = () => {
-  const [step, setStep] = useState("cart"); // cart | payment
+  const { products, getTotalCart } = useContext(CartContext);
+  const [step, setStep] = useState('cart'); // cart | payment
+  const [isValidDiscount, setIsValidDiscount] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
-  const t = useTranslations("Cart");
+  const t = useTranslations('Cart');
 
+  const createRandomNumberTransaction = () => {
+    return Math.floor(Math.random() * 1000000000);
+  };
+  const extractMessage = (str) => {
+    console.log(str);
+    if (!str) {
+      return t('error-not-found');
+    }
+    const match = str.match(/message:\s*(.+)/);
+    if (match) {
+      return match[1];
+    } else {
+      return t('error-not-found');
+    }
+  };
+
+  const sendEmail = async (
+    email = '',
+    phone = '',
+    interProducts = [],
+    idTransaction = ''
+  ) => {
+    const services = interProducts?.map((item) => item.name)?.join(', ');
+    const data = {
+      email_for_admin_data: {
+        client: pageName,
+        email: email,
+        name: nameCard,
+        amount: getTotalCart(),
+        phone_number: '+52' + phone,
+        service: services,
+        order_number: idTransaction,
+      },
+      email_for_client_data: {
+        email: email,
+      },
+    };
+    await ApiTransaction.sendEmail(data);
+  };
+
+  const onPaymentResult = async (data) => {
+    console.log('data', data);
+    setIsLoading(true);
+    await sleep(2000);
+    setIsLoading(false);
+
+    const merchantTransaction =
+      pageName + '-' + createRandomNumberTransaction();
+
+    const total = getTotalCart();
+    let body = {
+      merchant_transaction_id: merchantTransaction,
+      card: {
+        number: valueCard,
+        holder_name: data.cardName,
+        expiration_year: cardDate.slice(-2),
+        expiration_month: cardDate.substring(0, 2),
+        cvv: data.cardCvv,
+      },
+      customer: {
+        merchant_customer_id: '331415',
+        first_name: data.firstname,
+        second_name: '',
+        first_surname: data.lastname,
+        second_surname: data.secondSurname,
+        email: data.email,
+        phone_number: '+52' + data.phone,
+        home_address: {
+          street: data.street,
+          external_number: data.extNumber,
+          postal_code: data.codePostal,
+          colony: '',
+          city: data.city,
+          state_code: 'EM',
+          state_name: 'Mexico',
+          country_code: 'MX',
+          country_name: 'Mexico',
+        },
+        billing_address: {
+          street: data.street,
+          external_number: data.extNumber,
+          postal_code: data.codePostal,
+          colony: '',
+          city: data.city,
+          state_code: 'EM',
+          state_name: 'Mexico',
+          country_code: 'MX',
+          country_name: 'Mexico',
+        },
+        nationality: 'MX',
+        gender: 'male',
+      },
+      amount: 10,
+      currency: 'MXN',
+      description: 'Pago de evento',
+    };
+    await sleep(2000);
+    const dataRes = await ApiTransaction.makeTransaction(body);
+    setIsLoading(false);
+
+    if (dataRes?.content?.status === 'success') {
+      let idTransaction = dataRes?.content?.merchant_transaction_id;
+
+      sendEmail(data.email, data.phone, products, idTransaction);
+      return {
+        success: true,
+        data: idTransaction,
+      };
+    } else {
+      let message = extractMessage(dataRes?.content?.message?.detail);
+      return {
+        success: false,
+        data: message,
+      };
+    }
+  };
+  const onChangeDiscount = (value) => {
+    console.log('onChangeDiscount', value);
+  };
   return (
-    <div className="w-full flex justify-center mt-10 mb-20">
-      <div className="container px-4">
-        <div className="flex flex-col gap-5">
-          {step === "cart" && (
+    <div className='w-full flex justify-center mt-10 mb-20'>
+      <div className='container px-4'>
+        <div className='flex flex-col gap-5'>
+          {step === 'cart' && (
             <CartSection
-              onClickBuyMore={() => router.push("/#shop")}
-              onClickGoHome={() => router.push("/")}
-              variant="table"
+              onClickBuyMore={() => router.push('/#shop')}
+              onClickGoHome={() => router.push('/')}
+              variant='table'
               gridColumns={2}
             />
           )}
 
-          {step === "payment" && (
+          {step === 'payment' && (
             <Payments
-              callbackPayment={(data) => {
-                // Api
-                return;
-              }}
-              onClickBuyMore={() => router.push("/#shop")}
-              onClickGoHome={() => router.push("/")}
+              isValidDiscountCode={isValidDiscount}
+              handleChangeDiscountCode={onChangeDiscount}
+              onPaymentResult={onPaymentResult}
+              onClickBuyMore={() => router.push('/#shop')}
+              onClickGoHome={() => router.push('/')}
+              isLoading={isLoading}
             />
           )}
 
           <div>
             <Button
-              type="primary"
-              onClick={() => setStep(step === "cart" ? "payment" : "cart")}
+              type='primary'
+              onClick={() => setStep(step === 'cart' ? 'payment' : 'cart')}
             >
-              {step === "cart" ? "Go to Pay Form" : "Back to Cart Section"}
+              {step === 'cart' ? 'Go to Pay' : 'Back to Cart'}
             </Button>
           </div>
         </div>
